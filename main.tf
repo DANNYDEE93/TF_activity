9e2f40ede72b865ca021b90b7a73a5e929d392f5
Danielle Davis <140758597+DANNYDEE93@users.noreply.github.com>
2023-10-19 15:35:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,32 @@
+
 # configure aws provider
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
-  region = var.region
-}
-
-#connect to currently existing vpc named "Kura-VPC"
-resource "aws_vpc" "Kura-VPC" {
-  cidr_block       = var.vpc_cidr
-  instance_tenancy = "default"
+  region = var.region 
 }
 
 
+# launch instance in VPC
 
- #create instance and connect to Kura-VPC
+ #create instance
 resource "aws_instance" "TF_Activity" {
   ami = var.ami
   instance_type = var.instance_type
   associate_public_ip_address = true
+  subnet_id = var.subnet_id 
+  vpc_security_group_ids = [var.security_group_id]
 
   #user_data = ""
 
   tags = {
     Name : "TF_Activity"
-    vpc : "Kura-VPC"
+    vpc: "Kura-VPC"
     az : "${var.region}a"
   }
 }
 
-#print out instance ip address
+
 output "tf_activity_server" {
   value = aws_instance.TF_Activity.public_ip
 }
